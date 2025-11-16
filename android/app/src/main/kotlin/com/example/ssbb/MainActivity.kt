f0a35142be7026d98e1e47a5051ceee74d1432c1
Amaar Abd <abdamaar64@gmail.com>
2025-11-16 14:09:09 +0300

    create mvvm folders

---
+++ android/app/src/main/kotlin/com/example/ssbb/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ssbb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ssbb/features/Splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(debugShowCheckedModeBanner: false
+    ,home: SplashView(),);
+  }
+}
