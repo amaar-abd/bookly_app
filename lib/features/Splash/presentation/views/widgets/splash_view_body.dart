3e6e17f2e43d3fe4aa2e9e24c1e7f88ad09fe447
Amaar Abd <abdamaar64@gmail.com>
2025-11-18 01:06:02 +0300

    ading intanimation

--- lib/features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,29 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:ssbb/core/utils/assets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slideingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initslidingAnimation();
+  }
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      // crossAxisAlignment: CrossAxisAlignment.stretch,
+
       children: [
         Center(
           child: Stack(
             children: [
               Image.asset(AssetsData.logotwo, height: 300, width: 300),
               Positioned(
-                top: 230,
-                child: Center(
-                  child: Text(
-                    'مكتبتك الرقمية تبدأ من هنا',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'ciro',
+                top: 200,
+                left: 20,
+                child: AnimatedBuilder(
+                  animation: slideingAnimation,
+                  builder: (context, child) => SlideTransition(
+                    position: slideingAnimation,
+                    child: Text(
+                      'مكتبتك الرقمية تبدأ من هنا',
+
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w300,
+                        fontFamily: 'ciro',
+                      ),
                     ),
                   ),
                 ),
@@ -33,5 +58,17 @@ class SplashViewBody extends StatelessWidget {
         ),
       ],
     );
+  }
+   void initslidingAnimation() {
+    
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    slideingAnimation = Tween<Offset>(
+      begin: Offset(0, 2),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
   }
 }
