21aadf282777824c87faae4528b88c359a35eaf5
Amaar Abd <abdamaar64@gmail.com>
2025-11-26 22:21:21 +0300

    create NewsetBooksCubit

--- lib/features/home/presentation/maneger/newset books/newset_books_cubit.dart
+++ lib/features/home/presentation/maneger/newset books/newset_books_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:ssbb/features/home/data/models/book_model/book_model.dart';
+import 'package:ssbb/features/home/data/repos/home_repo.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) => emit(NewsetBooksFailuer(errorMesage: failure.errorMessege)),
+      (books) => emit(NewsetBooksSuccess(books: books)),
+    );
+  }
 }
