aa321309abd7bd448cd51375aec1bec5a5d47e4c
Amaar Abd <abdamaar64@gmail.com>
2025-11-30 00:13:33 +0300

    add CachedNetworkImage to custom item

--- lib/features/home/presentation/views/widgets/custome_item.dart
+++ lib/features/home/presentation/views/widgets/custome_item.dart
@@ -1,21 +1,21 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+
 class CustomeItem extends StatelessWidget {
   const CustomeItem({super.key, required this.imageUrl});
   final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .35, //Responsive
-      child: AspectRatio(
-        aspectRatio: 2.2 / 3,
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(imageUrl),
-              fit: BoxFit.fill,
-            ),
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * .35, //Responsive
+        child: AspectRatio(
+          aspectRatio: 2.2 / 3,
+          child: CachedNetworkImage(
+            imageUrl: imageUrl,
+            fit: BoxFit.fill,
+            errorWidget: (context, url, error) => Icon(Icons.error),
           ),
         ),
       ),
